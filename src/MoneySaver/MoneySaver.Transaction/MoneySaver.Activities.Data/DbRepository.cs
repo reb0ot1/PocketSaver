b0b0587dc67309eb7e770297b8f8d25dca174355
sbozhinov <sbojinov@yahoo.com>
2020-01-29 00:02:58 +0200

    Add DB context for activities api and add the projects to the IIS Manager

---
+++ src/MoneySaver/MoneySaver.Transaction/MoneySaver.Activities.Data/DbRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace MoneySaver.Activities.Data
+{
+    public class DbRepository<TEntity> : IRepository<TEntity>, IDisposable
+       where TEntity : class
+    {
+        private DbSet<TEntity> dbSet;
+        private readonly ActivitiesContext context;
+        public DbRepository(ActivitiesContext context)
+        {
+            this.context = context;
+            this.dbSet = context.Set<TEntity>();
+        }
+
+        public IQueryable<TEntity> All()
+        {
+            return this.dbSet;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            await this.dbSet.AddAsync(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            this.dbSet.Remove(entity);
+        }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return this.context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            this.context.Dispose();
+        }
+
+        public void Update(int id, TEntity entity)
+        {
+            this.dbSet.Update(entity);
+        }
+    }
+}
